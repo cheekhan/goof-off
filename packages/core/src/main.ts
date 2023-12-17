d68f0fa49b57080cccc784c4903fce7ef9bcd914
cheekhan <cheekhan@foxmail.com>
2023-12-17 19:51:53 +0800

    修复样式、store等

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,11 +1,14 @@
 import { element } from "@top/design"
 import { createApp } from "vue"
+import "./style/index.less"
 import AppPage from "./App.vue"
 import router from "./router"
+import store from "./store"
 
 
 
 createApp(AppPage)
+    .use(store)
     .use(router)
     .use(element)
     .mount('#app')
